2ca5325ecc0a0443b02b7a636fd40aed8f7aa089
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2017-10-06 21:10:17 -0300

    create obj assing in model Neg

--- JS-Cangaceiro-Livro/Modulo-01/client/app/domain/negociacao/Negociacao.js
+++ JS-Cangaceiro-Livro/Modulo-01/client/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,13 @@
 class Negociacao {
 
-    constructor(data, quantidade, valor) {
-        this._data = new Date(data);
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+
+        Object.assign(this, {
+            _quantidade,
+            _valor
+        });
+
+        this._data = new Date(_data);
 
         Object.freeze(this);
     }
