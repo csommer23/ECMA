b46ce168656ca972cb6e593a426a2704187d445b
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2017-10-07 14:08:47 -0300

    first solution for date

--- JS-Cangaceiro-Livro/Modulo-01/client/app/controllers/NegociacaoController.js
+++ JS-Cangaceiro-Livro/Modulo-01/client/app/controllers/NegociacaoController.js
@@ -11,8 +11,12 @@ class NegociacaoController {
     adicionar(event) {
         event.preventDefault();
     
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
+        let negociacao = new Negociacao(
+            this._inputData.value.replace(/-/g,','),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
     }
 }
