d40deca8b4947203ec106c57452a7af3905e25c1
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2017-10-22 22:06:39 -0200

    add class abstract

--- JS-Cangaceiro-Livro/Modulo-01/client/app/ui/views/NegociacoesView.js
+++ JS-Cangaceiro-Livro/Modulo-01/client/app/ui/views/NegociacoesView.js
@@ -1,12 +1,4 @@
-class NegociacoesView {
-
-    constructor(seletor) {
-        this._element = document.querySelector(seletor);
-    }
-
-    update(model) {
-        this._element.innerHTML = this.template(model);
-    }
+class NegociacoesView extends View {
 
     template(model) {
         return `
