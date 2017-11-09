c7600de5bc3ae6d5dd004cf1e85dfa47e6b37e4d
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2017-11-08 23:11:29 -0200

    add set in proxy

--- JS-Cangaceiro-Livro/Modulo-01/client/app/util/ProxyFactory.js
+++ JS-Cangaceiro-Livro/Modulo-01/client/app/util/ProxyFactory.js
@@ -3,7 +3,7 @@ class ProxyFactory {
     static create(objeto, props, armadilha) {        
         return new Proxy(objeto,{
             get(target, prop, receiver) {                    
-                if(typeof(target[prop]) == typeof(Function) && props.includes(prop)) {
+                if(ProxyFactory._ehFuncao(target[prop]) && props.includes(prop)) {
                     return function() {  
                         
                         target[prop].apply(target, arguments);
@@ -12,7 +12,19 @@ class ProxyFactory {
                 } else {                    
                     return target[prop];
                 }
+            },
+            set(target, prop, value, receiver) {
+                
+                const updated = Reflect.set(target,prop,value);
+
+                if(props.includes(prop)) armadilha(target);
+
+                return updated;
             }
         });
+    }
+
+    static _ehFuncao(fn) {
+        return typeof(fn) == typeof(Function)
     }
 }
