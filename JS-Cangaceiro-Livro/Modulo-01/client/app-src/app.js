56f04fbbb6374c4d0d43c4047aef4e8453c511fa
Cristiano Sommer <cristiano.silva.sommer@gmail.com>
2017-11-26 22:38:50 -0200

    add api fetch

--- JS-Cangaceiro-Livro/Modulo-01/client/app-src/app.js
+++ JS-Cangaceiro-Livro/Modulo-01/client/app-src/app.js
@@ -1,5 +1,19 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { Negociacao } from './domain/Index.js';
 
+
+const	negociacao = new Negociacao(new Date(), 1, 200);
+const	cabecalhos = new Headers();
+cabecalhos.set('Content-Type', 'application/json');
+
+const	config = {	
+      method: 'POST',
+      headers: cabecalhos,
+      body: JSON.stringify(negociacao)
+};
+
+fetch('/negociacoes', config)
+      .then(() => console.log('Dado enviado com sucesso'));
 
 const controller = new NegociacaoController(),
       $ = document.querySelector.bind(document);
